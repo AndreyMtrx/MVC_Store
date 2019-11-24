ea2d3955df89c6fbe575be6b0724883119527c7b
laazp08 <laazp08@gmail.com>
2019-11-25 01:51:58 +0300

    Добавьте файлы проекта.

---
+++ MVC_Store/Models/ViewModels/Pages/PageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MVC_Store.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.ViewModels.Pages
+{
+    public class PageViewModel
+    {
+        public PageViewModel()
+        {
+
+        }
+        public PageViewModel(PagesDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength:50,MinimumLength =3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue,MinimumLength =3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
